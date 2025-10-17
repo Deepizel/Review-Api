9953e232b4f2ea74b030eba9c5c96750cf172e22
Victor Ekundayo <rocmancave@gmail.com>
2025-10-17 22:22:01 +0100

    harmonized all settings and done

--- api/index.js
+++ api/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
+const cors = require('cors');
 const productRoutes = require('../routes/productRoutes');
 const errorMiddleware = require('../middlewares/errorMiddleware');
 
 dotenv.config();
 const port = process.env.PORT;
-
+const allowedOrigins = process.env.FRONTEND;
 // mongoose.connect(process.env.MONGODB_URI, {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true
@@ -21,7 +22,13 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 }).catch((err) => {
     console.log(err);
 });
-
+// this only allows requests from the localhost:3000
+const corsOptions = {
+    origin: allowedOrigins,
+    credentials: true,
+}
+// enable cors
+app.use(cors(corsOptions));
 // Middleware to use json
 app.use(express.json());
 // to use form data sending
